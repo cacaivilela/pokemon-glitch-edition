b928bf73e69e1fce143731e7a456e83b274e28e5
cacaivilela <henrique@tradeinsights.com>
2026-09-03 19:35:23 -0300

    fusao: LICKITE

--- src/data/fusoes-feitas.js
+++ src/data/fusoes-feitas.js
@@ -1331,5 +1331,33 @@ export const FUSOES_FEITAS = {
       "sprite": "assets/fusoes/cranidos+cubone~cranine.png",
       "lore": "CRANIDOS NA CABEÇA, CUBONE NO CORPO."
     }
+  ],
+  "lickitung+raticate": [
+    {
+      "id": "lickite",
+      "nome": "LICKITE",
+      "autor": "",
+      "tipos": [
+        "NORMAL"
+      ],
+      "inicial": {
+        "hp": 10,
+        "atk": 5,
+        "def": 5,
+        "spa": 5,
+        "spd": 5,
+        "spe": 5
+      },
+      "crescimento": {
+        "hp": 5,
+        "atk": 4.4,
+        "def": 5.4,
+        "spa": 1.5,
+        "spd": 2.1,
+        "spe": 4.4
+      },
+      "sprite": "assets/fusoes/lickitung+raticate~lickite.png",
+      "lore": "LICKITUNG NA CABEÇA, RATICATE NO CORPO."
+    }
   ]
 };
