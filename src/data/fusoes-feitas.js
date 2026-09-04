b8ab4eb4fbf807fc72d551824e7ac6799308fb27
cacaivilela <henrique@tradeinsights.com>
2026-09-04 06:42:54 -0300

    faxina: fora GENGAR

--- src/data/fusoes-feitas.js
+++ src/data/fusoes-feitas.js
@@ -1330,35 +1330,6 @@ export const FUSOES_FEITAS = {
       "lore": "LICKITUNG NA CABEÇA, RATICATE NO CORPO."
     }
   ],
-  "gengar+gengar": [
-    {
-      "id": "gengar",
-      "nome": "GENGAR",
-      "autor": "",
-      "tipos": [
-        "FANTASMA",
-        "VENENO"
-      ],
-      "inicial": {
-        "hp": 10,
-        "atk": 5,
-        "def": 5,
-        "spa": 5,
-        "spd": 5,
-        "spe": 5
-      },
-      "crescimento": {
-        "hp": 5,
-        "atk": 4.4,
-        "def": 5.4,
-        "spa": 1.5,
-        "spd": 2.1,
-        "spe": 4.4
-      },
-      "sprite": "assets/fusoes/gengar+gengar~gengar.png",
-      "lore": "GENGAR NA CABEÇA, GENGAR NO CORPO."
-    }
-  ],
   "kabutops+bellsprout": [
     {
       "id": "kabulsprout",
